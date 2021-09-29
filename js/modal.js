8f524a2a17c807844a58951e6ed9860f10541039
Anuar Shaidenov <anuarshaidenov@gmail.com>
2021-09-29 18:48:45 +0600

    add select modal option functionality

--- js/modal.js
+++ js/modal.js
@@ -4,6 +4,8 @@ const modalContainer = document.getElementById("modal-container");
 const btnModalOpen = document.getElementById("btn-modal-open");
 const btnModalClose = document.getElementById("modal-close");
 
+const selectModals = document.querySelectorAll(".modal-select");
+
 btnModalOpen.addEventListener("click", function () {
   [backdropModal, modalContainer].forEach((el) =>
     el.classList.remove("hidden")
@@ -13,5 +15,28 @@ btnModalOpen.addEventListener("click", function () {
 [backdropModal, btnModalClose].forEach((el) =>
   el.addEventListener("click", function () {
     [backdropModal, modalContainer].forEach((el) => el.classList.add("hidden"));
+  })
+);
+
+selectModals.forEach((section) =>
+  section.addEventListener("click", function () {
+    // Remove active states from all the sections
+    selectModals.forEach(function (section) {
+      const circle = section.querySelector(".circle");
+      const sectionPledge = section.querySelector(".section__pledge");
+
+      section.classList.remove("section--active");
+      circle.classList.remove("circle--active");
+
+      sectionPledge ? sectionPledge.classList.add("hidden") : "";
+    });
+
+    const circle = this.querySelector(".circle");
+    const sectionPledge = this.querySelector(".section__pledge");
+
+    this.classList.add("section--active");
+
+    circle.classList.add("circle--active");
+    sectionPledge.classList.remove("hidden");
   })
 );
